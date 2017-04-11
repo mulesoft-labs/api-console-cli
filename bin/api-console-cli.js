7dfdf58f076560ba4e8aa5b516d9e7bce19bf047
Pawel Psztyc <jarrodek@gmail.com>
2017-04-11 18:54:43 +0100

    Initial commit to have a repo outside my work computer

---
+++ bin/api-console-cli.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+'use strict';
+
+process.title = 'api-console';
+
+var semver = require('semver');
+// Early exit if the user's node version is too low.
+if (!semver.satisfies(process.version, '>=4')) {
+  console.log(
+      'API Console CLI requires at least Node v4. ' +
+      'You have ' + process.version + '.');
+  process.exit(1);
+}
+
+require('../lib/commands/run');
